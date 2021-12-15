e390b7abafa9af546d2deb7248f83ca0573e0438
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2021-12-16 02:37:00 +0800

    generate Category model and update db schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_181102) do
+ActiveRecord::Schema.define(version: 2021_12_15_183518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_12_15_181102) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "categories", "users"
 end
