b7c8825b87208cda2af8db88062a171daff0f05f
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2021-12-17 15:26:02 +0800

    fix validation of presence of boolean field in Task model

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -1,5 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :category
 
-  validates :category, :title, :completed, :urgent, :deleted, presence: true
+  validates :category, :title, presence: true
+  validates :completed, inclusion: [true, false]
+  validates :urgent, inclusion: [true, false]
+  validates :deleted, inclusion: [true, false]
 end
