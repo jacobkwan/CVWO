2633ea89662675482719deea399bc31b4e354ad0
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2021-12-17 15:26:27 +0800

    generate seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create(
+  [
+    {
+      name: 'jacob',
+      password: 'pw123'
+    },
+    {
+      name: 'john',
+      password: 'pw123'
+    }
+  ]
+)
+categories = users.first.categories.create(
+  [
+    {title: "General"},
+    {title: "School"}
+  ]
+)
+general = categories.first
+school = categories.second
+general_tasks = general.tasks.create!(
+  [
+    {
+      title: "Pay Bills",
+      completed: false,
+      urgent: true,
+      deleted: false
+    },
+    {
+      title: "Cook Dinner",
+      completed: true,
+      urgent: false,
+      deleted: false
+    }
+  ]
+)
+school_tasks = school.tasks.create!(
+  [
+    {
+      title: "Study for Test",
+      completed: false,
+      urgent: true,
+      deleted: false
+    },
+    {
+      title: "Do Tutorial",
+      completed: true,
+      urgent: false,
+      deleted: false
+    }
+  ]
+)
